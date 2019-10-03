0654dae897e9dd90698319f5a9854f82282c2831
renburnett <renardburnett@gmail.com>
2019-10-02 17:39:15 -0700

    fixed kexp api bug

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,6 +2,8 @@ require 'rest-client'
 require 'json'
 
 class SongsController < ApplicationController
+    before_action :security
+
     def index
         @songs = Song.all
     end
@@ -16,29 +18,37 @@ class SongsController < ApplicationController
         #nothing needed, just the api call button
     end
 
-    #create makes api calls for 20 recent songs from kexp, and their corresponding genres -> updates db
+    #create makes api calls for x_number of recent songs from kexp, and their corresponding genres -> updates db
     def create
         songs = get_kexp_songs()
         idx = 0
-        
-        #TODO: #songs['results'].length do    #should loop over the 20 song elements
-        5.times do
-            if songs['results'][idx]['playtype']['name'] != "Air break" && songs['results'][idx]['playtype']['name'] != "Air Break"
+
+        while idx < songs['results'].length
+            if songs['results'][idx]['playtype']['name'] != "Media play"
+                idx += 1
+                next
+            end
                 song_title = songs['results'][idx]['track']['name']
                 song_artist = songs['results'][idx]['artist']['name']
-                song_album = songs['results'][idx]['release']['name']
-                song_is_local = songs['results'][idx]['artist']['islocal']
-                song_release_year = songs['results'][idx]['releaseevent']['year'].to_i
                 
+                if !songs['results'][idx]['release']
+                    song_album = ""
+                    song_release_year = 0
+                else
+                    song_album = songs['results'][idx]['release']['name']
+                    song_release_year = songs['results'][idx]['releaseevent']['year'].to_i
+                end
+                
+                song_is_local = songs['results'][idx]['artist']['islocal']
                 #if song|artist|album don't exist -> create 
                 if Album.find_by(title: song_album) == nil
                     song_artist = song_artist.split('feat.')[0]
-                    mod_artist = song_artist.strip().split(' ').join('+')
+                    mod_artist = song_artist.strip().gsub(/[^0-9a-z ]/i, '').split(' ').join('+')
                     parsed_response = get_song_genre(mod_artist)
     
                     #if bad status code -> query by song instead
                     if parsed_response["status_code"] > 299 || parsed_response["status_code"] < 200 || parsed_response["genre"] == ""
-                        mod_song = song_title.strip().split(' ').join('+')
+                        mod_song = song_title.strip().gsub(/[^0-9a-z ]/i, '').split(' ').join('+')
                         parsed_response = get_song_genre(mod_song)
                     end
     
@@ -47,6 +57,7 @@ class SongsController < ApplicationController
                 else 
                     @album = Album.find_by(title: song_album)
                 end
+
                 if Artist.find_by(name: song_artist) == nil
                     @artist = Artist.new(name: song_artist, is_local?: song_is_local, album: @album)
                     @artist.save()
@@ -61,10 +72,8 @@ class SongsController < ApplicationController
                     @song = Song.find_by(title: song_title)
                     @song.update(artist: @artist)
                 end
-                
                 idx += 1
             end
-        end
         redirect_to songs_path
     end
 
@@ -78,29 +87,30 @@ class SongsController < ApplicationController
         url = "https://legacy-api.kexp.org/play/"
         response = RestClient.get(url)
         body = JSON.parse(response.body)
+        return body
     end
 
     def get_song_genre(song_or_artist)
         url = "http://api.musixmatch.com/ws/1.1/track.search?q_track=#{song_or_artist}&page_size=6&s_track_rating=desc&apikey=d724f77b3d9ea7b37948f7baecea0291"
         response = RestClient.get(url)
         body = JSON.parse(response.body)
-        idx = 0
+        i = 0
         parsed_response = {}
         parsed_response["status_code"] =  body["message"]["header"]["status_code"]
 
         if !body["message"]["body"]["track_list"].empty? #first annoying empty check
-            while body["message"]["body"]["track_list"][idx]["track"]["primary_genres"].empty?  #iterate until a non-empty genre is hit
-                idx += 1
+            while body["message"]["body"]["track_list"][i]["track"]["primary_genres"].empty?  #iterate until a non-empty genre is hit
+                i += 1
             end
         else 
-            parsed_response["genre"] = ""
+            parsed_response["genre"] = "Music"
             return parsed_response
         end
 
         begin
-            parsed_response["genre"] = body["message"]["body"]["track_list"][idx]["track"]["primary_genres"]["music_genre_list"][0]["music_genre"]["music_genre_name"]
+            parsed_response["genre"] = body["message"]["body"]["track_list"][i]["track"]["primary_genres"]["music_genre_list"][0]["music_genre"]["music_genre_name"]
         rescue
-            parsed_response["genre"] = ""
+            parsed_response["genre"] = "Music"
         end
 
         return parsed_response
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_002647) do
+ActiveRecord::Schema.define(version: 2019_10_02_172650) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "albums", force: :cascade do |t|
     t.string "title"
@@ -47,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_10_01_002647) do
     t.string "user_pass"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
